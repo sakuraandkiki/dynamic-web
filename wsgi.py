ef5bc99b75f8fa6ff617f7b2f8708d024d9fad87
sakuraandkiki <1789582317@qq.com>
2025-01-09 05:53:07 +0100

    wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,1 +1,11 @@
- 
+ from app import app
+import os
+
+if __name__ == "__main__":
+    # 确保数据库和表已创建
+    with app.app_context():
+        from extensions import db
+        db.create_all()
+    
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port
